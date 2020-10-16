4c02d6c782afba8fb5c984e5e4472fcce75cef48
AKen00 <ajken04@gmail.com>
2020-10-16 19:47:22 -0400

    set up paths to html in 'public'

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const port = 3003;
+const publicDir = path.join(__dirname, "/public");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(publicDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+
+});
+
